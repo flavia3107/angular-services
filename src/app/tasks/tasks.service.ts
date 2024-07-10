c5ec95fe414235fbf18787ecfe9772ea30b7d06f
Flavia Kosti <flaviakosti@gmail.com>
2024-07-09 21:58:41 -0400

    Learning-Services:Add service logic

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+
+import { Task } from './task.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
+  private tasks = signal<Task[]>([]);
 
-  constructor() { }
+  allTasks = this.tasks.asReadonly();
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN'
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 }
